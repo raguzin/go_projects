f9e06346d560448368ff5944d38f1c825e247a65
Aleksei Raguzin <a.raguzin83@gmail.com>
2020-10-23 22:58:05 +0300

    email_validate func

--- src/email_check/email_validate.go
+++ src/email_check/email_validate.go
@@ -7,36 +7,23 @@ import (
 )
 
 func email_validate(email string) {
-	if strings.Count(email, "@") == 1 {	// e-mail состоит из имени и доменной части, эти части разделяются символом "@"
+	if strings.Count(email, "@") == 1 {
 		email_substrings := strings.Split(email, "@")	
-		domain_substring := email_substrings[1]
-		domain_substring_pattern := `^(\w+(-*)\w+\.)+\w{2,}$`
-		if len(domain_substring) > 3 && len(domain_substring) < 256 {
-			matched, err := regexp.MatchString(domain_substring_pattern, domain_substring)
-			if matched == false {
-				fmt.Printf("доменная часть не соответствует условию")
+		if len(email_substrings[0]) > 0 && len(email_substrings[0]) < 128 && len(email_substrings[1]) > 3 && len(email_substrings[1]) < 256 {
+			email_pattern := `[_0-9a-z\-]*\.?[_0-9a-z\-]*("[_0-9a-z!:\-]*\.?[_0-9a-z!:\-]*")*[_0-9a-z\-]*\.?[_0-9a-z\-]*@(((([_0-9a-z]{1,})|([_0-9a-z]{1}[_0-9a-z-]*[_0-9a-z]{1}))\.){1,}[0-9a-z]{2,})`
+			matched, _ := regexp.MatchString(email_pattern, email)
+			if matched {
+				fmt.Printf("email корректный")
+			} else {
+				fmt.Printf("email не соответсвует шаблону")
 			}
-			//fmt.Println(matched)
-			//fmt.Println(err)
 		} else {
-			fmt.Printf("доменная часть короче 3 или длинее 256 символов")
+			fmt.Printf("длина email не соответсвует условиям")
 		}
-
-		name_substring := email_substrings[0]
-		name_substring_pattern := `\w*(-*)(\.)?"{2}`
-		if len(name_substring) > 128 {
-			matched, err := regexp.MatchString(name_substring_pattern, name_substring)
-			//if matched == false {
-			//	fmt.Printf("доменная часть не соответствует условию")
-			//}
-			fmt.Println(matched)
-			fmt.Println(err)
-		} else {
-			fmt.Printf("именная часть длинее 128 символов")
-		}
+	} else {
+		fmt.Printf("email содержит более одного символа @")
 	}
-}
-		
+}		
 		
 		
 
@@ -45,7 +32,4 @@ func main() {
 	fmt.Printf("Insert email for validate: ")
 	fmt.Scanf("%s\n", &email)
 	email_validate(email)
-
-//	fmt.Printf("You email - %s\n", email)
-
 }
